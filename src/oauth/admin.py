a07ef7f9f84ea1757d6f02adf60d07000e7db37d
serega-s <comp.it.05@inbox.ru>
2022-06-09 17:53:54 +0300

    added Django Model Follower

--- src/oauth/admin.py
+++ src/oauth/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.AuthUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'display_name', 'join_date']
+    list_display_links = ['email']
+
+
+# @admin.register(models.SocialLink)
+# class SocialLinkAdmin(admin.ModelAdmin):
+#     list_display = ['user', 'link']
--- src/oauth/models.py
+++ src/oauth/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
 
+from src.base.service import get_path_upload_avatar, validate_size_image
+
 
 class AuthUser(models.Model):
     """User model for platform
@@ -12,10 +14,10 @@ class AuthUser(models.Model):
     bio = models.CharField(max_length=2000, blank=True, null=True)
     display_name = models.CharField(max_length=30, blank=True, null=True)
     avatar = models.ImageField(
-        upload_to='',
+        upload_to=get_path_upload_avatar,
         blank=True,
         null=True,
-        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg'])]
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg']), validate_size_image]
     )
 
     """Always return True, lets us know if user is authenticated
@@ -26,3 +28,26 @@ class AuthUser(models.Model):
 
     def __str__(self):
         return f'{self.email}'
+
+
+class Follower(models.Model):
+    """Django ORM Model of followers
+    """
+    user = models.ForeignKey(
+        AuthUser, on_delete=models.CASCADE, related_name='owner')
+    subscriber = models.ForeignKey(
+        AuthUser, on_delete=models.CASCADE, related_name='subscribers')
+
+    def __str__(self):
+        return f'{self.subscriber} subscribed {self.user}'
+
+
+# class SocialLink(models.Model):
+#     """Model of user social networks
+#     """
+
+#     user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name='social_links')
+#     link = models.URLField(max_length=100)
+
+#     def __str__(self):
+#         return f'{self.user}'
