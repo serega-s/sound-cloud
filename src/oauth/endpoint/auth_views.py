9ce6bd947e809bacf37b682f54e9aef89e5d0037
serega-s <comp.it.05@inbox.ru>
2022-06-13 22:29:50 +0300

    Added views for auth

--- src/oauth/endpoint/auth_views.py
+++ src/oauth/endpoint/auth_views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import AuthenticationFailed
+
+from .. import serializer
+from ..services.google import check_google_auth
 
 
 def google_login(request):
@@ -6,3 +13,16 @@ def google_login(request):
     """
 
     return render(request, 'oauth/google_login.html')
+
+
+@api_view(['POST'])
+def google_auth(request):
+    """ Auth confirmation with Google
+    """
+    google_data = serializer.GoogleAuth(data=request.data)
+
+    if google_data.is_valid():
+        token = check_google_auth(google_data.data)
+        return Response(token)
+    else:
+        return AuthenticationFailed(code=status.HTTP_403_FORBIDDEN, data='Bad Google data')
