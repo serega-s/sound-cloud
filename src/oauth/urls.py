4d2c062a23433b4c820d26278c36d506c5434fb5
serega-s <comp.it.05@inbox.ru>
2022-06-13 22:30:35 +0300

    updated oauth urls

--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .endpoint import auth_views, views
 
 urlpatterns = [
-    path('', auth_views.google_login)
+    path('', auth_views.google_login, name='google_login'),
+    path('google/', auth_views.google_auth, name='google_auth')
 ]
