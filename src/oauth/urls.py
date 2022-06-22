3ce7e214e7c2887354d3c5444c6148af30120805
serega-s <comp.it.05@inbox.ru>
2022-06-22 15:27:45 +0300

    updated code style

--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -3,13 +3,38 @@ from django.urls import path
 from .endpoint import auth_views, views
 
 urlpatterns = [
-    path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
+    path(
+        'me/',
+        views.UserView.as_view(
+            {'get': 'retrieve', 'put': 'update'}
+        )
+    ),
 
-    path('author/', views.AuthorView.as_view({'get': 'list'})),
-    path('author/<int:pk>/', views.AuthorView.as_view({'get': 'retrieve'})),
+    path(
+        'author/',
+        views.AuthorView.as_view(
+            {'get': 'list'}
+        )
+    ),
+    path(
+        'author/<int:pk>/',
+        views.AuthorView.as_view(
+            {'get': 'retrieve'}
+        )
+    ),
 
-    path('social/', views.SocialLinkView.as_view({'get': 'list', 'post': 'create'})),
-    path('social/<int:pk>/', views.SocialLinkView.as_view({'put': 'update', 'delete': 'destroy'})),
+    path(
+        'social/',
+        views.SocialLinkView.as_view(
+            {'get': 'list', 'post': 'create'}
+        )
+    ),
+    path(
+        'social/<int:pk>/',
+        views.SocialLinkView.as_view(
+            {'put': 'update', 'delete': 'destroy'}
+        )
+    ),
 
     path('google/', auth_views.google_auth),
 
